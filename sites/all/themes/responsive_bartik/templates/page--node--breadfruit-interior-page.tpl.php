bbf81f9e2bde1123fa236a9bcb28083110b22882
Brent Delperdang <bdelperdang@backofficethinking.com>
2017-04-13 12:44:35 -0400

    Menu Improvements

--- sites/all/themes/responsive_bartik/templates/page--node--breadfruit-interior-page.tpl.php
+++ sites/all/themes/responsive_bartik/templates/page--node--breadfruit-interior-page.tpl.php
@@ -184,7 +184,12 @@
 <div class="section-wrap">
 
     <?php print $breadcrumb; ?>
-
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="title" id="page-title">
+          <?php print $title; ?>
+        </h1>
+      <?php endif; ?>
     <div id="content" class="column"><div class="section">
       <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
       
@@ -195,12 +200,7 @@
 		<?php endif; ?>
 	  
 	  <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title">
-          <?php print $title; ?>
-        </h1>
-      <?php endif; ?>
+
       <?php print render($title_suffix); ?>
       <?php if ($tabs): ?>
         <div class="tabs">
